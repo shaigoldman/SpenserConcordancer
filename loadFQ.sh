efc2a1e88ee4c851b0427434be13fab9aaafad9b
Shai Goldman <shaitzvi@gmail.com>
2022-12-26 21:06:58 -0500

    reorganize source texts

--- loadFQ.sh
+++ loadFQ.sh
@@ -1,4 +1,5 @@
 for i in {1..6}
 do
-	lynx -dump https://www.luminarium.org/renascence-editions/queene$i.html > queene$i.txt	
+	echo "loading book $i"
+	lynx -dump https://www.luminarium.org/renascence-editions/queene$i.html > resources/queene$i.txt	
 done
